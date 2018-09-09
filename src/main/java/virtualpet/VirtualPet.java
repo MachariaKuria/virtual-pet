d0c51606ee9789f28793b60b706e902d08787f19
MachariaKuria <skuria@student.cscc.edu>
2018-09-09 16:42:46 -0400

    initial setup

---
+++ src/main/java/virtualpet/VirtualPet.java
@@ -0,0 +1,71 @@
+package virtualpet;
+
+public class VirtualPet {
+
+	private int hungerLevel;
+	private int thirstLevel;
+	private int sleepLevel;
+
+	public int getHungerLevel() {
+		return hungerLevel;
+	}
+
+	public int getThirstLevel() {
+
+		return thirstLevel;
+	}
+
+	public int getSleepLevel() {
+
+		return sleepLevel;
+	}
+
+	public void reduceHunger(int food) {
+		hungerLevel -= food;
+	}
+
+	public void reduceThirst(int drink) {
+		thirstLevel -= drink;
+
+	}
+
+	public void reduceSleep(int sleep) {
+
+		sleepLevel -= sleep;
+
+	}
+
+	public void tick() {
+
+		hungerLevel += 20;
+		thirstLevel += 15;
+		sleepLevel += 10;
+
+	}
+	public void increaseHunger(int food) {
+		
+		hungerLevel += food;
+		
+	}
+	
+	public void increaseThirst(int drink) {
+		
+		thirstLevel += drink;
+		
+	}
+	
+	public void increaseSleep(int sleep) {
+		
+		sleepLevel += sleep;
+		
+	}
+
+	public VirtualPet(int hungerLevel, int thirstLevel, int sleep) {
+
+		this.hungerLevel = hungerLevel;
+		this.thirstLevel = thirstLevel;
+		this.sleepLevel = sleep;
+	}
+
+
+}
---
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -0,0 +1,115 @@
+package virtualpet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class VirtualPetTest {
+
+	VirtualPet underTest = new VirtualPet(100, 100, 100);
+
+	@Test
+	public void hungerShouldGoDownBy5WhenFedBy5() {
+
+		underTest.reduceHunger(5);
+		int check = underTest.getHungerLevel();
+		assertEquals(check, 95);
+	}
+
+	@Test
+	public void thirstShouldGoDownBy5WhenDrinkBy5() {
+
+		underTest.reduceThirst(5);
+		int check = underTest.getThirstLevel();
+		assertEquals(check, 95);
+	}
+
+	@Test
+	public void sleepinessShouldGoDownBy5() {
+
+		underTest.reduceSleep(5);
+		int check = underTest.getSleepLevel();
+		assertEquals(check, 95);
+	}
+
+	@Test
+	public void hungerShouldGoUpBy5WhenNotFedBy5() {
+
+		underTest.increaseHunger(5);
+		int check = underTest.getHungerLevel();
+		assertEquals(check, 105);
+	}
+
+	@Test
+	public void thirstShouldGoUpBy5WhenNoDrinkBy5() {
+
+		underTest.increaseThirst(5);
+		int check = underTest.getThirstLevel();
+		assertEquals(check, 105);
+	}
+
+	@Test
+	public void sleepinessShouldGoUpBy5() {
+
+		underTest.increaseSleep(5);
+		int check = underTest.getSleepLevel();
+		assertEquals(check, 105);
+	}
+
+	@Test
+	public void HungerShouldGoUpWith1Tick() {
+
+		underTest.tick();
+		int check = underTest.getHungerLevel();
+		assertEquals(check, 120);
+
+	}
+
+	@Test
+	public void HungerShouldGoUpWith2Ticks() {
+
+		underTest.tick();
+		underTest.tick();
+		int check = underTest.getHungerLevel();
+		assertEquals(check, 140);
+
+	}
+
+	@Test
+	public void ThirstShouldGoUpWith1Tick() {
+
+		underTest.tick();
+		int check = underTest.getThirstLevel();
+		assertEquals(check, 115);
+
+	}
+
+	@Test
+	public void ThirstShouldGoUpWith2Ticks() {
+
+		underTest.tick();
+		underTest.tick();
+		int check = underTest.getThirstLevel();
+		assertEquals(check, 130);
+
+	}
+
+	@Test
+	public void SleepShouldGoUpWith1Tick() {
+
+		underTest.tick();
+		int check = underTest.getSleepLevel();
+		assertEquals(check, 110);
+
+	}
+
+	@Test
+	public void SleepShouldGoUpWith2Ticks() {
+
+		underTest.tick();
+		underTest.tick();
+		int check = underTest.getSleepLevel();
+		assertEquals(check, 120);
+
+	}
+}
